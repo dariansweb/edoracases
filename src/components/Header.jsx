68a99d977153e1ed2451b9bb8be4b1feef0084ba
dariansweb <dariansweb@gmail.com>
2024-09-29 19:17:42 -0500

    full width containers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
   };
 
   return (
-    <header className="header-bar">
+    <header className="header-bar full-width-container">
       <div className="header-left">
         <span
           className={`star-icon ${isExploding ? "explode-effect" : ""}`}
