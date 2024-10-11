75b8bfa71747a5122947523b9fc062cef79f790c
dariansweb <dariansweb@gmail.com>
2024-10-10 20:13:56 -0500

    Footer rewwork - size was too much - spread out menu and rearrange logo legals

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,77 +3,96 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer-bar">
-      <div className="footer-left"></div>
-      <nav className="footer-nav">
-      <hr /><hr />        
-        <h3 className="light">Explore</h3>
-        <hr /><hr />
-        <ul className="footerListItems">
-          <li className="footerListItems">
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/prototypes">Prototypes</a>
-          </li>
-          <li>
-            <a href="https://www.github.com/dariansweb/edoracases">GitHub</a>
-          </li>          
-        </ul>
-        <hr /><hr />
-        <h3 className="light">Programs</h3>
-        <hr /><hr />          
-        <ul>
-          <li>
-            <a href="/child-and-family">Child & Family</a>
-          </li>
-          <li>
-            <a href="/aging">Aging</a>
-          </li>
-          <li>
-            <a href="/disability">Disability</a>
-          </li>
-          <li>
-            <a href="/juvenile-justice">Juvenile Justice</a>
-          </li>
-          <li>
-            <a href="/medical">Medical</a>
-          </li>
-          <li>
-            <a href="/provider-services">Provider Services</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="footer-left">
+        <nav className="footer-nav">
+          <hr />
+          <hr />
+          <h3 className="light">Explore</h3>
+          <hr />
+          <hr />
+          <ul>
+            <li className="footer-links">
+              <a href="/">Home</a>
+            </li>
+            <li className="footer-links">
+              <a href="/about">About</a>
+            </li>
+            <li className="footer-links">
+              <a href="/services">Services</a>
+            </li>
+            <li className="footer-links">
+              <a href="/contact">Contact</a>
+            </li>
+            <li className="footer-links">
+              <a href="/prototypes">Prototypes</a>
+            </li>
+            <li className="footer-links">
+              <a href="https://www.github.com/dariansweb/edoracases">GitHub</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div className="footer-center">
+        <nav className="footer-nav">
+          <hr />
+          <hr />
+          <h3 className="light">Programs</h3>
+          <hr />
+          <hr />
+          <ul>
+            <li className="footer-links">
+              <a href="/child-and-family">Child & Family</a>
+            </li>
+            <li className="footer-links">
+              <a href="/aging">Aging</a>
+            </li>
+            <li className="footer-links">
+              <a href="/disability">Disability</a>
+            </li>
+            <li className="footer-links">
+              <a href="/juvenile-justice">Juvenile Justice</a>
+            </li>
+            <li className="footer-links">
+              <a href="/medical">Medical</a>
+            </li>
+            <li className="footer-links">
+              <a href="/provider-services">Provider Services</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="footer-right">
         <div className="social-icons">
           <a
+            className="footer-link"
             href="https://www.linkedin.com/in/darianmross/"
             aria-label="LinkedIn"
           >
             <span>LinkedIn</span>
           </a>
-          <a href="divmailto:info@edoracases.com" aria-label="Email">
+          <a
+            className="footer-link"
+            href="divmailto:info@edoracases.com"
+            aria-label="Email"
+          >
             <span>Email</span>
           </a>
-          <a href="https://www.edoracases.com" aria-label="Website">
+          <a
+            className="footer-link"
+            href="https://www.edoracases.com"
+            aria-label="Website"
+          >
             <span>EDORA</span>
           </a>
         </div>
-        {/* <div className="footer-search">
-          <input type="text" placeholder="Search EDORA..." />
-          <button>🔍</button>
-        </div> */}
-      </div>
-
-      <div className="footer-right">
+        <div className="footer-back-to-top">
+          <a href="#top" className="back-to-top-show">
+            ↑ Back to Top
+          </a>
+        </div>
+        <hr />
+        <hr />
         <span className="footer-icon">🌟</span> {/* Star icon for branding */}
         <span className="footer-brand">EDORA</span> {/* Brand name */}
         &copy; {new Date().getFullYear()} EDORA, All rights reserved. .{" "}
@@ -86,11 +105,8 @@ const Footer = () => {
             Terms of Service
           </a>
         </div>
-        <div className="footer-back-to-top">
-          <a href="#top" className="back-to-top-show">
-            ↑ Back to Top
-          </a>
-        </div>
+        <hr />
+        <hr />
       </div>
     </footer>
   );
