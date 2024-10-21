bf7ff5c519cd48f2b3c9525707d2c747f8831581
dariansweb <dariansweb@gmail.com>
2024-10-21 13:21:14 -0500

    client page 4 is in skeleton stages

---
+++ src/data/clientmanagement.js
@@ -0,0 +1,24 @@
+const clientManagement = [
+    "Demographics",
+    "Intake",
+    "Case Notes",    
+    "Services",
+    "Documents",
+    "Classifications",
+    "Affiliations",
+    "Placements",
+    "Contractual Services",
+    "Treatment Plans",
+    "Appointments",
+    "Financial Information",
+    "Assessments",
+    "Family Relationships",
+    "Eligibility",
+    "Notifications",
+    "Interventions",
+    "Outcome Tracking",
+    "Legal Information",
+    "Goals and Progress"
+  ];
+  
+  export default clientManagement;
